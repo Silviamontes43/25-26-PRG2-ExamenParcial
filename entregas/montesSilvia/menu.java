7a17452ac357672645cd000ebfd3d94c2348f1a0
Silviamontes43 <silvia.montes@alumnos.uneatlantico.es>
2026-04-06 18:05:51 +0200

    menu

---
+++ entregas/montesSilvia/menu.java
@@ -0,0 +1,33 @@
+class Menu {
+    private String entrante;
+    private String principal1;
+    private String principal2;
+    private String postre;
+
+    private double precioEntrante;
+    private double precioPrincipal1;
+    private double precioPrincipal2;
+    private double precioPostre;
+
+    public Menu(String entrante, String principal1, String precioPrincipal2, String postre){
+                  this.entrante = entrante;
+                  this.principal1 = principal1;
+                  this.principal2 = principal2;
+                  this.postre = postre;
+      
+    }
+    public menuEntrante(ensaladaCesar, 12) {
+    return entrante;
+    }
+    public menuPrincipal1 (fileteTernera, 25) {
+        return principal;
+    }
+    public menuPrincipal2(paella, 22) {
+        return principal2;
+    }
+    public postre (flanHuevo, 6) {
+        return postre;
+    }
+
+   
+}
