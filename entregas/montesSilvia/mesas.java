d2840d6fa16d8d949660feaced3f5eecebf5b036
Silviamontes43 <silvia.montes@alumnos.uneatlantico.es>
2026-04-06 17:50:17 +0200

    clase mesas

---
+++ entregas/montesSilvia/mesas.java
@@ -0,0 +1,18 @@
+class Elrestaurante {
+    class Mesas {
+    private String mesa;  
+    
+
+    public Mesas(String mesa1, String mesa2, String mesa3) {
+        this.mesa1 = mesa1;
+        this.mesa2=mesa2;
+        this.mesa3= mesa3;
+    }
+
+    public String mesas() {
+        return mesas;
+    }
+
+   
+}
+}
